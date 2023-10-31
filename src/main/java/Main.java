1e690223a37bfd1c1888a085cf03d8f15a33235b
PeterBurrfot <arr.in.the.pw@gmail.com>
2023-10-31 18:39:26 +0300

    TravelTime

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        final int DISTANCE_FROM_HOME_TO_OFFICE_KM = 50;
+        final double AVERAGE_TAXI_SPEED_KM = 60.5;
+        final double TRAVEL_TIME = DISTANCE_FROM_HOME_TO_OFFICE_KM / AVERAGE_TAXI_SPEED_KM;
+
+        final double SLOWING_TAXI_EVENING = 1.5;
+        final double FROM_OFFICE_TO_HOME_KM = TRAVEL_TIME * SLOWING_TAXI_EVENING;
+
+        System.out.println("Время от дома до офиса: " + TRAVEL_TIME);
+        System.out.println("Время от офиса до дома: " + DISTANCE_FROM_HOME_TO_OFFICE_KM);
+    }
+}
